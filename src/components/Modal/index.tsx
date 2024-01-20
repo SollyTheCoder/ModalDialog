3afc4a07f52106d875c231455f4a0f0c01592066
solomon <solomon.owenwilliams@vodafone.com>
2024-01-20 19:34:34 +0000

    refactor: change z-index value

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -19,9 +19,9 @@ export default function Modal({
 }: ModalProps) {
   return createPortal(
     <>
-      <div className="opacity-50 fixed w-full h-full top-0 start-0 bg-slate-100" />
-      <div className="fixed w-full flex h-full items-center align-middle justify-center text-center top-0 start-0">
-        <div className=" rounded-lg z-10 w-96 max-h-96 px-10 py-5 bg-white overflow-scroll">
+      <div className="opacity-50 fixed w-full h-full top-0 start-0 bg-slate-100 z-40" />
+      <div className="fixed w-full z-50 flex h-full items-center align-middle justify-center text-center top-0 start-0">
+        <div className=" rounded-lg w-96 max-h-96 px-10 py-5 bg-white overflow-scroll">
           <div id="modal-header" className="flex w-full justify-between">
             <p />
             <p className="w-auto text-2xl">{title}</p>
